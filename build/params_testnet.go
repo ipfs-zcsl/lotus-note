ede6843e82cec0043752c3cd1edc68da25399e7c
Cory Schwartz <cory@protocol.ai>
2020-09-08 11:01:05 -0700

    reset for drand test

--- build/params_testnet.go
+++ build/params_testnet.go
@@ -16,10 +16,11 @@ const UpgradeBreezeHeight = 41280
 const BreezeGasTampingDuration = 120
 
 func init() {
-	power.ConsensusMinerMinPower = big.NewInt(10 << 40)
+	power.ConsensusMinerMinPower = big.NewInt(1024 << 20)
 	miner.SupportedProofTypes = map[abi.RegisteredSealProof]struct{}{
-		abi.RegisteredSealProof_StackedDrg32GiBV1: {},
-		abi.RegisteredSealProof_StackedDrg64GiBV1: {},
+		abi.RegisteredSealProof_StackedDrg512MiBV1: {},
+		abi.RegisteredSealProof_StackedDrg32GiBV1:  {},
+		abi.RegisteredSealProof_StackedDrg64GiBV1:  {},
 	}
 }
 

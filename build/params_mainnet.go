1bb050fb8b800b67fb9c4727382c53d73f6aea90
Cory Schwartz <cory@protocol.ai>
2020-11-17 17:18:20 -0800

    reset butterfly

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -15,35 +15,34 @@ import (
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0:                  DrandIncentinet,
-	UpgradeSmokeHeight: DrandMainnet,
+	0: DrandIncentinet,
 }
 
-const UpgradeBreezeHeight = 41280
+const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 
-const UpgradeSmokeHeight = 51000
+const UpgradeSmokeHeight = -2
 
-const UpgradeIgnitionHeight = 94000
-const UpgradeRefuelHeight = 130800
+const UpgradeIgnitionHeight = -3
+const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(138720)
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 140760
+const UpgradeTapeHeight = 60
 
 // This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
 // Miners, clients, developers, custodians all need time to prepare.
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
-const UpgradeLiftoffHeight = 148888
+const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 170000
+const UpgradeKumquatHeight = 90
 
 // TODO: Height??
 const UpgradeCalicoHeight = 999999
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInDay * 2)
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 30))
 
 	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
 		SetAddressNetwork(address.Mainnet)
@@ -53,7 +52,7 @@ func init() {
 		UpgradeActorsV2Height = math.MaxInt64
 	}
 
-	Devnet = false
+	Devnet = true
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
